ac5acd7c0f3e763fba9574e9e2191cd7cd2f1535
miaomiaobupang <40153049+miaomiaobupang@users.noreply.github.com>
2018-06-20 09:12:40 +0800

    20180620
    我的博客

---
+++ blog/myblog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,'myblog/index.html',{'title':'啊啊啊啊啊','content':'宝宝宝宝不'})
